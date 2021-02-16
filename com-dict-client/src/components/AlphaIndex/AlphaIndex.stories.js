66fde9cb514cc7da357a68fb953776fc1ce2cedc
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-16 18:04:12 +0530

    add storybook

---
+++ com-dict-client/src/components/AlphaIndex/AlphaIndex.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import AlphaIndex from "./index";
+
+export default {
+  title: "AlphaIndex",
+  component: AlphaIndex,
+};
+export const Primary = () => <AlphaIndex />;
